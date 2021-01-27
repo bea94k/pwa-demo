d645981fd7c4888870edf4329bee35ea821d236f
bea94k <beatakusnierz94@gmail.com>
2021-01-27 10:00:04 +0200

    Add Camera to Home, rename Camera folder

--- src/Camera/Camera.js
+++ src/Camera/Camera.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./Camera.css";
 
 const Camera = () => {
     const [camerasList, setCamerasList] = useState([]);
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Camera from '../Camera/Camera';
 
 export default function Home() {
     return (
         <div>
             Home here
+            <Camera />
         </div>
     )
 }
