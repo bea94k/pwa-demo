9b851e96257f6d5734777d68eb4fc55da1e6b8c7
bea94k <beatakusnierz94@gmail.com>
2021-01-27 10:59:20 +0200

    Saving post data in state

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddPost = () => {
+    const [post, setPost] = useState({
+        photo: '',
+        title: '',
+        location: ''
+    });
+
+    const handleInputChange = (e) => {
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const addNewPost = (newPost) => {
+        console.log(newPost);
+    }
+
     return (
         <div>
             <h1>Add your post</h1>
             <p>Photo here</p>
-            <input onChange={() => {console.log('tadaaa')}} name="title" type="text" placeholder="Title" />
-            <input onChange={() => {console.log('tadaaammmm')}} name="location" type="text" placeholder="Location" />
-            <p className="btn">ADD</p>
+            <input onChange={handleInputChange} name="title" type="text" placeholder="Title" />
+            <input onChange={handleInputChange} name="location" type="text" placeholder="Location" />
+            <p className="btn" onClick={() => addNewPost(post)}>ADD</p>
         </div>
     );
 }
