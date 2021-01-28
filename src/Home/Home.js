b5b2d6e2d8e9aad62ca6b83a24a7237f3102be3b
Flowry <flowry@Flowrys-MacBook-Pro.local>
2021-01-28 12:43:06 +0200

    Merged branches

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import Camera from '../Camera/Camera';
 import Preview from '../Preview/Preview';
 import AddPost from '../AddPost/AddPost';
+
 export default function Home() {
     const [photoBlob, setPhotoBlob] = useState();
     const [photoAccepted, setPhotoAccepted] = useState(false);
+    
     const handlePhotoBlob = (newPhotoBlob) => {
         setPhotoBlob(newPhotoBlob);
     }
@@ -13,7 +15,6 @@ export default function Home() {
     }
     return (
         <div>
-            Home here
                 {/* <p>Photo blob exists: {photoBlob ? 'yes' : 'no'}</p>
                 <p>Photo is accepted: {photoAccepted.toString()}</p> */}
                 {!photoBlob ? (
